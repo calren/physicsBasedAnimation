689d3d1e64e0b45f54b83ed9a72a2914139997e4
Caren Chang <calren24@gmail.com>
2017-04-25 22:45:12 -0700

    Add slideractivity with value adjustments

--- app/src/main/java/com/example/caren/physicsbasedpulltorefresh/SliderActivity.java
+++ app/src/main/java/com/example/caren/physicsbasedpulltorefresh/SliderActivity.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.animation.DynamicAnimation;
 import android.support.animation.SpringAnimation;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
-import static android.support.animation.SpringForce.DAMPING_RATIO_HIGH_BOUNCY;
+import static android.support.animation.SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY;
 import static android.support.animation.SpringForce.STIFFNESS_VERY_LOW;
 
-public class MainActivity extends Activity {
+public class SliderActivity extends Activity {
 
     private static final int FINAL_Y_POSITION = 500;
     private View button;
@@ -18,35 +19,40 @@ public class MainActivity extends Activity {
     private SpringAnimation springAnim;
     private float originalButtonTranslation;
 
+    private SeekBar bounceSeekBar;
+    private SeekBar stiffnessSeekBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_slider);
 
         ghostImage = findViewById(R.id.image);
         button = findViewById(R.id.button);
+        bounceSeekBar = (SeekBar) findViewById(R.id.bounceValueAdjustment);
+        stiffnessSeekBar = (SeekBar) findViewById(R.id.stiffnessValueAdjustment);
+
         originalButtonTranslation = button.getTranslationY();
 
-        button.setOnTouchListener(new View.OnTouchListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getActionMasked() == MotionEvent.ACTION_UP) {
-                    long pressTime = event.getEventTime() - event.getDownTime();
-                    resetAnimation(pressTime / 100);
-                    springAnim.start();
-                }
-                return true;
+            public void onClick(View view) {
+                resetAnimation();
+                springAnim.start();
             }
         });
     }
 
-    private void resetAnimation(float stiffness) {
+    private void resetAnimation() {
+        Log.i("Caren", "b : " + (bounceSeekBar
+                .getProgress() / 200f));
         springAnim =
                 new SpringAnimation(ghostImage, DynamicAnimation.TRANSLATION_Y, FINAL_Y_POSITION);
-        springAnim.getSpring().setDampingRatio(DAMPING_RATIO_HIGH_BOUNCY);
-        springAnim.getSpring().setStiffness(stiffness > 1 ?
-                stiffness * STIFFNESS_VERY_LOW :
-                1 * STIFFNESS_VERY_LOW);
+        springAnim.getSpring().setStiffness(STIFFNESS_VERY_LOW * (stiffnessSeekBar.getProgress() / 2));
+//        springAnim.getSpring().setDampingRatio(bounceSeekBar.getProgress() / 200);
+        springAnim.getSpring().setDampingRatio(DAMPING_RATIO_MEDIUM_BOUNCY - (bounceSeekBar
+                .getProgress() / 200f));
+
 
         springAnim.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
             @Override
@@ -57,3 +63,4 @@ public class MainActivity extends Activity {
         });
     }
 }
+
